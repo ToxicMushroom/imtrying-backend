e5187427135a9b564a7351c2e1dee1da05a4781e
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2020-11-17 12:08:57 +0100

    refactor: switch to kts buildfiles chore(deps): updated kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,80 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("application")
+    id("com.github.johnrengelman.shadow") version "6.1.0"
+    id("org.jetbrains.kotlin.jvm") version "1.4.10"
+    id("com.apollographql.apollo") version "2.4.1"
+}
+
+application {
+    mainClassName = "me.melijn.siteapi.MelijnSiteKt"
+}
+group = "me.melijn.siteapi"
+version = "0.0.3"
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_15
+    targetCompatibility = JavaVersion.VERSION_15
+}
+
+repositories {
+    jcenter()
+    maven { url = uri("https://jitpack.io") }
+    mavenCentral()
+}
+
+dependencies {
+    // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-coroutines-core
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.1")
+
+    // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-coroutines-jdk8
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.4.1")
+
+    // https://mvnrepository.com/artifact/ch.qos.logback/logback-classic
+    implementation("ch.qos.logback:logback-classic:1.2.3")
+
+    // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-core
+    implementation("com.fasterxml.jackson.core:jackson-core:2.11.3")
+
+    // https://github.com/FasterXML/jackson-module-kotlin
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.11.3")
+
+    // https://mvnrepository.com/artifact/io.ktor/ktor-client-cio
+    implementation("io.ktor:ktor:1.4.1")
+    implementation("io.ktor:ktor-client-okhttp:1.4.1")
+    implementation("io.ktor:ktor-server-netty:1.4.1")
+
+    // https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-stdlib-jdk8
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.10")
+
+    // https://github.com/jwtk/jjwt
+    // https://mvnrepository.com/artifact/io.jsonwebtoken/jjwt-impl
+    implementation("io.jsonwebtoken:jjwt-impl:0.11.2")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.11.2")
+
+    // https://search.maven.org/artifact/com.zaxxer/HikariCP
+    implementation("com.zaxxer:HikariCP:3.4.5")
+
+    // implementation("com.github.husnjak:IGDB-API-JVM:0.7")
+    implementation("io.lettuce:lettuce-core:5.3.4.RELEASE")
+
+    // https://github.com/cdimascio/dotenv-java
+    implementation("io.github.cdimascio:java-dotenv:5.3.1")
+}
+
+tasks {
+    withType(JavaCompile::class) {
+        options.encoding = "UTF-8"
+    }
+    withType(KotlinCompile::class) {
+        kotlinOptions {
+            // 15 not supported at this time 25/10/2020 | latest kotlin: 1.4.10
+            jvmTarget = "14"
+        }
+    }
+
+    shadowJar {
+        archiveFileName.set("melijn-site-backend.jar")
+    }
+}
