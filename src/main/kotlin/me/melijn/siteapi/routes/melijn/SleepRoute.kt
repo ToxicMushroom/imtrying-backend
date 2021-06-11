05ef4fea03af8acb3459193d47da173e897b39fb
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2021-06-11 22:54:25 +0200

    feat: sleeproute

--- src/main/kotlin/me/melijn/siteapi/routes/melijn/SleepRoute.kt
+++ src/main/kotlin/me/melijn/siteapi/routes/melijn/SleepRoute.kt
@@ -1,5 +1,6 @@
 package me.melijn.siteapi.routes.melijn
 
+import io.ktor.client.request.*
 import me.melijn.siteapi.router.AbstractRoute
 import me.melijn.siteapi.router.IRouteContext
 import me.melijn.siteapi.router.get
@@ -11,17 +12,19 @@ class SleepRoute : AbstractRoute("/sleep") {
     }
 
     override suspend fun execute(context: IRouteContext) {
+        val resList = mutableListOf<String>()
         for (podId in 0 until context.podInfo.podCount) {
             val url = "${context.melijnHostPattern.replace("{podId}", "$podId")}/shutdown"
-            val resList = mutableListOf<String>()
             try {
-                val res = context.get<String>(url)
+                val res = context.get<String>(url) {
+                    header("Authorization", context.settings.melijnApi.token)
+                }
                 resList.add("$url -> $res")
             } catch (t: Throwable) {
                 resList.add("$url -> null")
                 null
             } ?: continue
-            context.reply("Slept all")
         }
+        context.reply("$resList")
     }
 }
