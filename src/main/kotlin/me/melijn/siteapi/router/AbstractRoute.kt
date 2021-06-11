80bc9e9e41588bf85cb184024a61837f2b590843
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2021-06-11 22:36:04 +0200

    feat: log auth

--- src/main/kotlin/me/melijn/siteapi/router/AbstractRoute.kt
+++ src/main/kotlin/me/melijn/siteapi/router/AbstractRoute.kt
@@ -1,6 +1,7 @@
 package me.melijn.siteapi.router
 
 import io.ktor.http.*
+import org.slf4j.LoggerFactory
 
 
 abstract class AbstractRoute(val route: String, val httpMethod: HttpMethod = HttpMethod.Get) {
@@ -8,6 +9,7 @@ abstract class AbstractRoute(val route: String, val httpMethod: HttpMethod = Htt
     var authorization: Boolean = false
     var rateLimiter: RateLimiter? = null
     var requiredParameters = emptyArray<String>()
+    private val logger = LoggerFactory.getLogger(this::class.java)
 
     suspend fun run(context: IRouteContext) {
         context.init()
@@ -16,6 +18,7 @@ abstract class AbstractRoute(val route: String, val httpMethod: HttpMethod = Htt
 
         // AuthorizationCheck
         if (authorization && context.headers["Authorization"] != context.settings.restServer.authorization) {
+            logger.info("${context.headers["Authorization"]} vs ${context.settings.restServer.authorization}")
             context.replyError(HttpStatusCode.Unauthorized)
             return
         }
